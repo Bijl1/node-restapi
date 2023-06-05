d076c5894d638a352025322b07b36306042eb374
Jonathan Bijl <sannoayo@gmail.com>
2023-06-05 13:44:38 -0400

    Seach for a movie in the list

--- index.js
+++ index.js
@@ -37,5 +37,18 @@ app.post("/movie", (req, res) => {
     res.send("Movie is added to the list!");
 });
 
+//Seach for a movie in the list
+app.get('/movie/:id', (req, res) => {
+    const id = req.params.id
+
+    for (let movie of movies) {
+        if(movie.id === id) {
+            res.json(movie)
+            return
+        }
+    }
+    res.status(404).send('Movie not found')
+})
+
 //set the server to listen at port
 app.listen(port, () => console.log(`Server listening at port ${port}`));
