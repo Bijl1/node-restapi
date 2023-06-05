f8dc5d60a0388535849d23fbeee26dd839f36318
Jonathan Bijl <sannoayo@gmail.com>
2023-06-05 13:18:41 -0400

    Movie list

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 //index.js
-const express = require('express')
+const express = require("express")
 
 const app = express()
-const port = 3000
+const port = 3000;
 
 let movies = [
   {
     id: "1",
     title: "Inception",
     director: "Christopher Nolan",
-    release_date: "2010-07-16"
+    release_date: "2010-07-16",
   },
   {
     id: "2",
     title: "The Irishman",
     director: "Martin Scorses",
-    release_date: "2019-09-27"
+    release_date: "2019-09-27",
   },
 ];
 
 //get the movie list in the form of JSON
+app.get("/movie", (req, res) => {
+    res.json(movies);
+});
+
+//set the server to listen at port
+app.listen(port, () => console.log(`Server listening at port ${port}`));
